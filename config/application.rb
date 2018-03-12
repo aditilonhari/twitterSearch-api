48072ce04dc0cbd2fd712fb4c9bb73c3e3e46ab8
Aditi Lonhari <lonhari.aditi@gmail.com>
2018-03-11 23:45:19 -0700

    Adding github origin to handle requests from frontend through CORS

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module TwitterSearchApi
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3000', 'https://aditilonhari.github.io'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
